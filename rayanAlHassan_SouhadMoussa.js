78dcc9e8853051a1ea7fbc9e44031af97e328a3d
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-12 16:46:57 +0300

    new ex 6 for ex2  with funct

--- rayanAlHassan_SouhadMoussa.js
+++ rayanAlHassan_SouhadMoussa.js
@@ -17,7 +17,7 @@
 // }
 // ex2 
 
-// function findMinMax() {
+
 //     let Arr = [50, 70, 20, 10, 13];
 //     let minValue = Infinity;
 //     let maxValue = -Infinity;
@@ -35,8 +35,8 @@
 
 //     console.log("Minimum element is:" + minValue);
 //     console.log("Minimum element is:" + maxValue);
-// }
-// findMinMax();
+// 
+
 
 //ex 3
 // var n; // decleare a variable that take the nb of line 
@@ -89,17 +89,40 @@
 // console.log(result)
 
 //ex6 with function
-// ex 3
-function displayGraph(){
- var n; // decleare a variable that take the nb of line 
-n=parseInt(prompt("enter nb of line"))
+// ex 3  with funct
+// function displayGraph(){
+//  var n; // decleare a variable that take the nb of line 
+// n=parseInt(prompt("enter nb of line"))
 
-    for(let i=1; i<=n ; i++){ // nb of line
-        for(let j =1 ; j<=i; j++){  // nb of column
-console.log("*")
-        }
-        console.log("\n")
-    }
-}
+//     for(let i=1; i<=n ; i++){ // nb of line
+//         for(let j =1 ; j<=i; j++){  // nb of column
+// console.log("*")
+//         }
+//         console.log("\n")
+//     }
+// }
 
-displayGraph()
+// displayGraph()
+
+
+//ex2 with funct
+// function findMinMax() {
+//     let Arr = [50, 70, 20, 10, 13];
+//     let minValue = Infinity;
+//     let maxValue = -Infinity;
+
+//     for (let i=0 ; i<Arr.length-1;i++) {
+
+//         // Find minimum value
+//         if (Arr[i] < minValue)
+//             minValue = Arr[i] ;
+
+//         // Find maximum value
+//         if (Arr[i]  > maxValue)
+//             maxValue = Arr[i] ;
+//     }
+
+//     console.log("Minimum element is:" + minValue);
+//     console.log("Minimum element is:" + maxValue);
+// }
+// findMinMax();
