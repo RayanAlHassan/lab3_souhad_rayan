a19660196850395fa5ab527ab0531d0a4fbee80f
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-12 16:59:23 +0300

    new ex 6 for ex4

--- rayanAlHassan_SouhadMoussa.js
+++ rayanAlHassan_SouhadMoussa.js
@@ -52,33 +52,20 @@
 // ex 4
 
 
-// var array=[0,5,2,9,15,7,19,50,80]
+var array=[50, 70, 20, 10, 13]
 
-// var max=array[0]
-// for (let i=0;i<array.length; i++){
-//     for(let j=0;j<array.length; j++){
-//         if(array[i] > array[j]){
-//             max=array[i]
-//         }
-//     }
-// }
-//  console.log("max number is "+max)
-//   let newArr = [];
-
-//     for(let i = 0; i < Array.length; i++) {
-//         let temp = arr[i];
-//         let x = 0;
-//         for(let j = 0; j < temp.length; j++) {
-
-//             if(temp[j] > x) {
-//                 x = temp[j];
-//             }
-//         }
-
-//         newArr.push(x);
-//         //console.log(arr[i])
-//     }
-//     console.log(newArr);
+var max=array[0]
+for (let i=0;i<array.length; i++){
+    for(let j=i+1;j<array.length; j++){
+        if(array[j] > max){
+            max=array[j]
+        }
+    }
+}
+ console.log("max number is "+max)
+ 
+     
+   
 
 //ex 5
 // function sumOfSquares(num) {
@@ -131,23 +118,23 @@
 // ex 1 with funct 
 // //ex1  with funct
 
-function assignKey(){
-var keys ;
-keys=parseInt(prompt("enter the nb of keys"))
-switch (keys) {
-  case 0:
-    console.log("we sank together");
-    break;
-  case 3000: // Value of foo matches this criteria; execution starts from here
-    console.log("we did it");
-  // Forgotten break! Execution falls through
-  case keys>0&&keys<3000: // no break statement in 'case 0:' so this case will run as well
-    console.log("we need to swim a little bit more");
-    break; // Break encountered; will not continue into 'case 2:'
+// function assignKey(){
+// var keys ;
+// keys=parseInt(prompt("enter the nb of keys"))
+// switch (keys) {
+//   case 0:
+//     console.log("we sank together");
+//     break;
+//   case 3000: // Value of foo matches this criteria; execution starts from here
+//     console.log("we did it");
+//   // Forgotten break! Execution falls through
+//   case keys>0&&keys<3000: // no break statement in 'case 0:' so this case will run as well
+//     console.log("we need to swim a little bit more");
+//     break; // Break encountered; will not continue into 'case 2:'
  
-  default:
-    console.log("default");
-}
-}
-assignKey()
+//   default:
+//     console.log("default");
+// }
+// }
+// assignKey()
 
