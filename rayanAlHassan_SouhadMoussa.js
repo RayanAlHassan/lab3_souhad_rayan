92295ab8ed0db46e9b9de187a9893df7f3b13f3a
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-09-12 16:28:55 +0300

    new ex1

--- rayanAlHassan_SouhadMoussa.js
+++ rayanAlHassan_SouhadMoussa.js
@@ -1,20 +1,20 @@
-// //ex1
-// var keys ;
-// keys=parseInt(prompt("enter the nb of keys"))
-// switch (keys) {
-//   case 0:
-//     console.log("we sank together");
-//     break;
-//   case 3000: // Value of foo matches this criteria; execution starts from here
-//     console.log("we did it");
-//   // Forgotten break! Execution falls through
-//   case keys>0&&keys<3000: // no break statement in 'case 0:' so this case will run as well
-//     console.log("we need to swim a little bit more");
-//     break; // Break encountered; will not continue into 'case 2:'
+//ex1
+var keys ;
+keys=parseInt(prompt("enter the nb of keys"))
+switch (keys) {
+  case 0:
+    console.log("we sank together");
+    break;
+  case 3000: // Value of foo matches this criteria; execution starts from here
+    console.log("we did it");
+  // Forgotten break! Execution falls through
+  case keys>0&&keys<3000: // no break statement in 'case 0:' so this case will run as well
+    console.log("we need to swim a little bit more");
+    break; // Break encountered; will not continue into 'case 2:'
  
-//   default:
-//     console.log("default");
-// }
+  default:
+    console.log("default");
+}
 // ex2 
 
 // function findMinMax() {
@@ -39,13 +39,28 @@
 // findMinMax();
 
 //ex 3
-var n;
-n=parseInt(prompt("enter nb of line"))
+// var n; // decleare a variable that take the nb of line 
+// n=parseInt(prompt("enter nb of line"))
 
-    for(let i=1; i<=n ; i++){
-        for(let j =1 ; j<=i; j++){
-console.log("*")
-        }
-        console.log("\n")
-    }
+//     for(let i=1; i<=n ; i++){ // nb of line
+//         for(let j =1 ; j<=i; j++){  // nb of column
+// console.log("*")
+//         }
+//         console.log("\n")
+//     }
+
+//ex 4
+
+// var arr =  [50, 70, 20, 80, 13];
+// var max=arr[0] ;
+// for(let i=0; i<arr.length; i++){
+//  for(let j=0; j<arr.length; j++){
+//   if(max[i] > max[j]){
+//     max=arr[i]
+//   }
+//  }
+
+
+// }
+//  console.log(max)
 
